8bc287544f7cf0c06c1e5bb6f35e35126c71724e
Maciej Wojda <mwx27@protonmail.com>
2024-11-21 16:23:43 +0100

    edited comment

--- ignition/modules/FundraiseAdapter.ts
+++ ignition/modules/FundraiseAdapter.ts
@@ -5,7 +5,8 @@ const FundraiseAdapterModule = buildModule("FundraiseAdapterModule", (m) => {
   const oldContractAddress = m.getParameter<string>(
     "oldContractAddress",
     "0x0000000000000000000000000000000000000000" 
-    // Default value ^above^, to be replaced here or with CLI parameter: --parameter oldContractAddress=0x123...
+    // Default value ^above^, to be replaced
+    // Important note: to deploy new contract with other address, journal.jsonl needs to be removed first
   );
 
   // Deployment
